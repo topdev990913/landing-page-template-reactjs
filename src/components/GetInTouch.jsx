bb77e526097ed38892ea76818ab6ef396a83b524
PETER990913 <jeremy.t.1132@gmail.com>
2023-05-24 06:06:11 -0700

    'update'

---
+++ src/components/GetInTouch.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {  
+    Button,
+    Stack,
+} from '@mui/material'
+import Title from './Title'
+import Paragraph from './Paragraph'
+import { Link } from 'react-router-dom'
+
+const GetInTouch = () => {
+
+    return (
+        <Stack 
+        component='section'
+        direction="column"
+        justifyContent= 'center'
+        alignItems='center'
+        sx={{
+            py: 10,
+            mx: 6,
+        }}
+        >
+            <Title 
+            text={
+                'Contact us to buy property'
+                } 
+            textAlign={'center'}
+            />
+            <Paragraph 
+            text={
+                'It is our commitment to ensure a professional and enjoyable \
+                new home buying experience for you. \
+                If you want to get a home to start living as a family in an \
+                area that you love click the button below.'
+            }
+            maxWidth = {'sm'}
+            mx={0}
+            textAlign={'center'}
+            />
+            <Button component={Link} 
+            to={'/contact'}
+            variant="contained" 
+            type="submit"
+            size="medium"
+            sx= {{ 
+                fontSize: '0.9rem',
+                textTransform: 'capitalize', 
+                py: 2,
+                px: 4,
+                mt: 3, 
+                mb: 2,
+                borderRadius: 0,
+                backgroundColor: '#14192d',
+                "&:hover": {
+                    backgroundColor: '#1e2a5a',
+                }
+            }}
+            >
+                get in touch
+            </Button>
+ 
+        </Stack>
+    )
+}
+
+export default GetInTouch;
